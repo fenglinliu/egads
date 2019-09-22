2fc2ab0afb85c511f5db62a81d97419dbf098c20
flynn <958953250@qq.com>
2019-09-22 17:48:07 +0800

    jython

---
+++ src/test/java/indi/flynn/ad/JythonTest.java
@@ -0,0 +1,13 @@
+package indi.flynn.ad;
+
+/**
+ * JythonTest
+ *
+ * @author Flynn
+ * @version 1.0
+ * @description Jython使用demo
+ * @email liufenglin@163.com
+ * @date 2019/9/22
+ */
+public class JythonTest {
+}
