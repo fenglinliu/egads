a897e123e03815edeaae0553f369a01bbb3740e3
flynn <958953250@qq.com>
2019-09-23 14:57:55 +0800

    Jython Test

--- src/test/java/indi/flynn/ad/JythonTest.java
+++ src/test/java/indi/flynn/ad/JythonTest.java
@@ -1,5 +1,10 @@
 package indi.flynn.ad;
 
+import org.python.util.PythonInterpreter;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * JythonTest
  *
@@ -10,4 +15,14 @@ package indi.flynn.ad;
  * @date 2019/9/22
  */
 public class JythonTest {
+    public static void main(String[] args) {
+//        PythonInterpreter pyInterp = new PythonInterpreter();
+//        for (int i = 0; i < map.size(); i++) {
+//            map.get(i).get
+//        }
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.exec("print('hello')");
+        interpreter.exec("print('Hello Python World!')");
+
+    }
 }
